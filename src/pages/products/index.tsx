78d546e8bf65488f6ed784860f5bf1e8e66900e4
enriqueorea <enriqueorea28@gmail.com>
2023-03-07 16:57:03 -0600

    FIX loading state in products page

--- src/pages/products/index.tsx
+++ src/pages/products/index.tsx
@@ -7,20 +7,22 @@ import { ProductList } from "~/components/Products";
 import type { Product } from "~/interfaces/Product.interface";
 
 const ProductsPage: NextPage = () => {
-	const { data, isFetching, isError } = api.products.getProducts.useQuery();
+	const { data, isFetching, isLoading } = api.products.getProducts.useQuery();
 
 	return (
 		<MainLayout title="Products" description="Products">
 			{isFetching ? (
-				<ThreeDots
-					height="80"
-					width="80"
-					radius="9"
-					color="#000000"
-					ariaLabel="three-dots-loading"
-					wrapperStyle={{}}
-					visible={true}
-				/>
+				<div className="absolute top-1/2 left-1/2">
+					<ThreeDots
+						height="80"
+						width="80"
+						radius="9"
+						color="#b1b1b1"
+						ariaLabel="three-dots-loading"
+						wrapperStyle={{}}
+						visible={true}
+					/>
+				</div>
 			) : (
 				<ProductList products={data as Product[]} />
 			)}
